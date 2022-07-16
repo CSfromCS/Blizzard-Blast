d3ed94e5e1c521d55f61156d0ede049be2ea9b30
Christian Sarabia <csfromcs@gmail.com>
2022-07-16 20:23:23 +0800

    Create html for data

--- order/urls.py
+++ order/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="order"),
+    # path('', views.index, name="order"),
+    path('slip/',views.orderslip, name="orderslip")
 ]
--- order/views.py
+++ order/views.py
@@ -1,9 +1,21 @@
 from django.http import HttpRequest
 from django.shortcuts import render, HttpResponse
 
-from order.models import OrderSlip
+from order.models import *
 
 # Create your views here.
 def index(request):
     o = OrderSlip(customer_name="Anne")
     return HttpResponse("Hello Blizzarder! You are in order.")
+
+def orderslip(request):
+    NewOrderSlip = OrderSlip(customer_name="Anne")
+    context = {
+        'Recipe' : Recipe.objects.all(),
+        'Ingredient' : Ingredient.objects.all(),
+        'NewOrderSlip' : NewOrderSlip,
+        'OrderSlip' : OrderSlip.objects.all(),
+        'OrderItem' : OrderItem.objects.all(),
+        'ItemAddOn' : ItemAddOn.objects.all()
+    }
+    return render(request, 'orderslip.html', context=context)
